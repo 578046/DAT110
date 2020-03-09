3288b8704ea966bcbd1515b49ced701a3190c3b1
Sondre Melhus <sondre.melhus@gmail.com>
2020-03-09 17:37:03 +0100

    Fyllt inn kode i Task C, DisplayDevice og TempratureDevice. Testing gjennstår.

--- src/no/hvl/dat110/iotsystem/DisplayDevice.java
+++ src/no/hvl/dat110/iotsystem/DisplayDevice.java
@@ -1,9 +1,12 @@
 package no.hvl.dat110.iotsystem;
 
+import no.hvl.dat110.broker.Storage;
 import no.hvl.dat110.client.Client;
 import no.hvl.dat110.messages.Message;
 import no.hvl.dat110.messages.PublishMsg;
 import no.hvl.dat110.common.TODO;
+import no.hvl.dat110.messagetransport.Connection;
+import no.hvl.dat110.messagetransport.MessagingClient;
 
 public class DisplayDevice {
 	
@@ -23,7 +26,16 @@ public class DisplayDevice {
 		// - receive messages on the topic
 		// - unsubscribe from the topic
 		// - disconnect from the broker
-		
+
+		Client c = new Client("displayDevice", Common.BROKERHOST,  Common.BROKERPORT );
+
+		c.connect();
+		c.createTopic("temperature");
+		c.subscribe("temperature");
+		c.receive();
+		c.unsubscribe("temperature");
+		c.disconnect();
+
 		// TODO - END
 		
 		System.out.println("Display stopping ... ");
--- src/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -1,7 +1,9 @@
 package no.hvl.dat110.iotsystem;
 
+import no.hvl.dat110.broker.ClientSession;
 import no.hvl.dat110.client.Client;
 import no.hvl.dat110.common.TODO;
+import no.hvl.dat110.messagetransport.MessagingClient;
 
 public class TemperatureDevice {
 
@@ -19,6 +21,16 @@ public class TemperatureDevice {
 		// - connect to the broker
 		// - publish the temperature(s)
 		// - disconnect from the broker
+
+		Client t = new Client("tempraturSensor", Common.BROKERHOST,  Common.BROKERPORT );
+
+		t.connect();
+
+		for (int i = 0; i<= COUNT; i++){
+			String temp = Integer.toString(sn.read());
+			t.publish("temperature", temp);
+		}
+		t.disconnect();
 
 		// TODO - end
 
